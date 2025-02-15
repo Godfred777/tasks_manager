ad32fe5d3e94faf0395753c8927d6ed912e64b08
Godfred777 <81054545+Godfred777@users.noreply.github.com>
2025-02-15 21:52:35 +0000

    Add authentication module with controller and service; integrate GraphQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule],
+  imports: [PrismaModule, UsersModule, GraphQLModule.forRoot<ApolloDriverConfig>({
+    driver: ApolloDriver,
+    playground: false,
+    autoSchemaFile: true,
+  }), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
